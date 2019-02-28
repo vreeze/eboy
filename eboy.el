6a124237c82c12fb4e87cde53cabbbc716c1b313
Pieter de Vreeze <mail@de-vreeze.com>
2019-02-28 22:39:02 +0100

    Add top banner

--- eboy.el
+++ eboy.el
@@ -1,5 +1,32 @@
 ;;; eboy.el ---  Emulator  -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2019 Pieter de Vreeze <mail@de-vreeze.com>
+
+;; Author: Pieter de Vreeze <mail@de-vreeze.com>
+;; URL: https://github.com/vreeze/eboy
+;; Version: 0.0.1
+;; Package-Requires:
+;; Keywords: games
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING. If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+;; This is a Gameboy emulator for Emacs -> eboy
 ;;; Commentary:
 (eval-when-compile (require 'cl))
 ;;(require 'cl-lib)
