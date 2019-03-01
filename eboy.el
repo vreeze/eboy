789c36e81f47b735fabd31435a897aa98243cdc9
Vasilij Schneidermann <mail@vasilij.de>
2019-03-01 22:15:11 +0100

    Move macros to separate file

--- eboy.el
+++ eboy.el
@@ -33,6 +33,7 @@
 
 
 ;;; Code:
+(require 'eboy-macros)
 ;;(load "eboy-cpu.el")
 (require 'eboy-cpu)
 
@@ -109,41 +110,12 @@
 (defvar eboy-joypad-direction-keys #xF "The direction keys of the joypad, |Down|Up|Left|Right| (0=pressed).")
 (defvar eboy-joypad-button-keys #xF "The button keys of the joypad, |Start|Select|B|A| (0=pressed).")
 
-(defmacro eboy-add-byte (byte value)
-  "Simulate byte behavior: add VALUE to BYTE."
-  `(progn (setq ,byte (+ ,byte ,value))
-          (setq ,byte (logand ,byte #xFF))))
-
-(defmacro eboy-add-to-short (short value)
-  "Simulate short behavior: add VALUE to SHORT."
-  `(progn (setq ,short (+ ,short ,value))
-          (setq ,short (logand ,short #xFFFF))))
-
 (defun eboy-byte-to-signed (byte)
   "Convert BYTE to signed number."
   ;;(* (1+ (logxor byte #xFF)) -1)
   (if (not (zerop (logand #x80 byte)))
       (setq byte (- byte #x100)))
   byte)
-
-(defmacro eboy-dec (reg flags)
-  "CPU Instruction: Decrement register REG and update FLAGS."
-  `(progn (eboy-add-byte ,reg -1)
-          (eboy-set-flag ,flags ,:Z (= ,reg 0))
-          (eboy-set-flag ,flags ,:N t)
-          (eboy-set-flag ,flags ,:H (= (logand ,reg #xF) #xF))))
-
-(defmacro eboy-inc-rpair (r1 r2 value)
-  "Increase register pair R1 R2 with VALUE."
-  `(let ((rpair (eboy-get-rpair ,r1 ,r2)) )
-     (eboy-add-to-short rpair ,value)
-     (eboy-set-rpair ,r1 ,r2 rpair)))
-
-(defmacro eboy-set-rpair (r1 r2 value)
-  "Put VALUE into register pair R1 and R2."
-  `(progn
-    (setq ,r1 (logand (lsh ,value -8) #xff))
-    (setq ,r2 (logand ,value #xff))))
 
 (defun eboy-get-rpair (r1 r2)
   "Get short by combining byte register R1 and R2."
